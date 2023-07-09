476deb06dd9afff2e7f46e8b17523068582f21b4
olawanle temitayo <olawanletemitayo@gmail.com>
2023-07-10 00:46:07 +0100

    added a preloader for better usee experience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,27 +3,78 @@ import { GlobalStyle } from '@/utils/GlobalStyle';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ContactUs, Footer, Header } from '@/components/UI';
-import Navbar from '@/components/General/Navbar';
-// import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
+import Preloader from '@/components/General/Preloader';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // // a preloader can be added here
-  // const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 4000);
-  // }, []);
-  return (
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+  return loading ? (
     <>
-      <GlobalStyle />
-      {/* 
-      <Preloader loading={loading as boolean} /> */}
-      <Header />
-      <Component {...pageProps} />
-      <ContactUs />
-      <Footer />
+      <motion.div
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+            y: 100,
+          },
+          pageAnimate: {
+            opacity: 1,
+            y: 0,
+          },
+          pageExit: {
+            opacity: 0,
+            y: -100,
+          },
+        }}
+      >
+        <GlobalStyle />
+        <Preloader />
+      </motion.div>
     </>
+  ) : (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        transition={{
+          type: 'tween',
+          ease: 'anticipate',
+          duration: 1.2,
+        }}
+        variants={{
+          pageInitial: {
+            opacity: 0,
+            clipPath: 'circle(0% at 100% 0%)',
+          },
+          pageAnimate: {
+            opacity: 1,
+            clipPath: 'circle(100% at 100% 0%)',
+          },
+          pageExit: {
+            clipPath: 'circle(0% at 100% 0%)',
+          },
+        }}
+      >
+        <GlobalStyle />
+
+        <Header />
+        <Component {...pageProps} />
+        <ContactUs />
+        <Footer />
+      </motion.div>
+    </AnimatePresence>
   );
 }
